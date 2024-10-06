ac60be2e39d2e220df3fb792e2c09f7058b9cd21
ByAlu <emrebelikirik4664@gmail.com>
2024-10-06 12:53:03 +0300

    db connection

---
+++ backend/db_connection.py
@@ -0,0 +1,21 @@
+from pymongo import MongoClient
+from configparser import ConfigParser
+
+config = ConfigParser()
+config.read('config.ini')
+
+psw = config.get('481-db','psw')
+connection_string = config.get('481-db','connection_string')
+
+def connect_to_db(collection_name = "reviews"):
+    try:
+        client = MongoClient(connection_string)
+        db = client["Intern-insider"]
+        collection = db[collection_name]
+        print(f"Connected to {collection_name} collection")
+        return collection
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+    
+connect_to_db()
